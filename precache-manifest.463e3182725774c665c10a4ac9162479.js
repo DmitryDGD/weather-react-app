2dcfe6d22a636a3740267bab6ee300c286b9de28
DmitryDGD <am.xhexm@gmail.com>
2020-06-28 21:26:23 +0300

    Updates

---
+++ precache-manifest.463e3182725774c665c10a4ac9162479.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd660aeecd6456dfb9fb04190c6e95f3",
+    "url": "/git@github.com:DmitryDGD/weather-react-app/index.html"
+  },
+  {
+    "revision": "77dfac7fd873ec2b1424",
+    "url": "/git@github.com:DmitryDGD/weather-react-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ff309e68b0f8f5c397e1",
+    "url": "/git@github.com:DmitryDGD/weather-react-app/static/css/main.bf41e6e2.chunk.css"
+  },
+  {
+    "revision": "77dfac7fd873ec2b1424",
+    "url": "/git@github.com:DmitryDGD/weather-react-app/static/js/2.39ec3617.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/git@github.com:DmitryDGD/weather-react-app/static/js/2.39ec3617.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff309e68b0f8f5c397e1",
+    "url": "/git@github.com:DmitryDGD/weather-react-app/static/js/main.71c741b6.chunk.js"
+  },
+  {
+    "revision": "29813786788189a06dbd",
+    "url": "/git@github.com:DmitryDGD/weather-react-app/static/js/runtime-main.8b32a4e3.js"
+  },
+  {
+    "revision": "804be1a64a03e39617b92629cfe1a14a",
+    "url": "/git@github.com:DmitryDGD/weather-react-app/static/media/polygon.804be1a6.jpg"
+  }
+]);
